8188095a63cf2511de8ed9aa89385a097be2bdfd
dannyvarod_cp <dd5a0369e14211a385f128a25405b173a470e69f7n6KD4hn>
2014-12-15 22:56:21 +0000

    Created NuGet package for deploying NuGetPack.exe + VSIX extension for template package project.
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs27]$/pubcomp/Building;C30191

---
+++ NuGetPack.NuGet/tools/Init.ps1
@@ -0,0 +1,28 @@
+﻿param($installPath, $toolsPath, $package)
+
+$solutionNode = Get-Interface $dte.Solution ([EnvDTE80.Solution2])
+
+$solutionItemsNode = $solutionNode.Projects | where-object { $_.ProjectName -eq ".NuGetPack" } | select -first 1
+
+if (!$solutionItemsNode) {
+	$solutionItemsNode = $solutionNode.AddSolutionFolder(".NuGetPack")
+}
+
+$solutionItemsProjectItems = Get-Interface $solutionItemsNode.ProjectItems ([EnvDTE.ProjectItems])
+
+$rootDir = (Get-Item $installPath).parent.parent.fullname
+
+$deploySource = join-path $installPath '\sln\'
+$deployTarget = join-path $rootDir '\.NuGetPack\'
+
+New-Item -ItemType Directory -Force -Path $deployTarget
+
+ls $deploySource | foreach-object {
+	
+	$targetFile = join-path $deployTarget $_.Name
+	
+	Copy-Item $_.FullName $targetFile -Recurse -Force
+
+	$solutionItemsProjectItems.AddFromFile($targetFile) > $null
+
+} > $null
